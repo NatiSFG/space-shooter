6743e38cb20b884259168efda0c93319b9d581bd
Natalia DaLomba <natalia@starforce.games>
2023-10-06 23:34:28 -0400

    EnemyHealthEntity class

--- Assets/Scripts/Enemies/EnemyHealthEntity.cs
+++ Assets/Scripts/Enemies/EnemyHealthEntity.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealthEntity : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class EnemyHealthEntity : MonoBehaviour {
+    [Header("Shield Protection")]
+    [SerializeField] private SpriteRenderer shield;
+    [SerializeField] private int totalShieldProtection = 1;
+    [SerializeField] private int currentShieldProtection;
+
+    [SerializeField] Enemy enemy;
+
+
+    public int CurrentShieldProtection {
+        get { return currentShieldProtection; }
+        set { currentShieldProtection = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        currentShieldProtection = totalShieldProtection;
+    }
+
+    public DamageResult TryDamage() {
+        if (TryDamageShield())
+            return DamageResult.ShieldDamaged;
+        else if (enemy.CheckToDefeatFromPlayer())
+            return DamageResult.Success;
+    }
+
+    public bool TryDamageShield() {
+        if (CurrentShieldProtection == totalShieldProtection) {
+            TakeShieldDamage();
+            return true;
+        } else return false;
+    }
+
+    private void TakeShieldDamage() {
+        CurrentShieldProtection--;
+        shield.gameObject.SetActive(false);
     }
 }
