37057da667c9b05f0f254ead93ba41a07df32710
Natalia DaLomba <natalia@starforce.games>
2023-10-15 23:32:50 -0400

    Spinner enemy has a shield with one health when they spawn.  When player shoots laser or collides with Spinner enemy while Spinner enemy has shield up, the shield gets destroyed. After that, the Spinner enemy is vulnerable fixes SPA-25

---
+++ Assets/Scripts/Enemies/EnemyShield.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShield : MonoBehaviour {
+    [Header("Shield Protection")]
+    [SerializeField] private int currentShieldProtection;
+    [SerializeField] private int totalShieldProtection = 1;
+    [SerializeField] private SpriteRenderer shield;
+
+    public bool IsShieldActive => shield.enabled == true;
+
+    public int CurrentShieldProtection {
+        get { return currentShieldProtection; }
+        set { currentShieldProtection = value; }
+    }
+
+    private void Start() {
+        currentShieldProtection = totalShieldProtection;
+    }
+
+    public bool TryDamageShield() {
+        if (IsShieldActive) {
+            TakeShieldDamage();
+            return true;
+        } else return false;
+    }
+
+    private void TakeShieldDamage() {
+        CurrentShieldProtection--;
+        shield.enabled = false;
+    }
+}
