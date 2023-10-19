017bba86afa429a48cb869d6d378f57d4fee9f61
Natalia DaLomba <natalia@starforce.games>
2023-10-19 08:27:01 -0400

    Midway implementing enemy aggression

--- Assets/Scripts/Controllers/Enemies/EnemyController2D.cs
+++ Assets/Scripts/Controllers/Enemies/EnemyController2D.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController2D : MonoBehaviour {
-    [SerializeField] protected float speed = 4;
+    [SerializeField] private float currentSpeed;
+    [SerializeField] protected float standardSpeed = 3f;
+    [SerializeField] private float aggressionSpeedMultiplier = 1.25f;
+    [SerializeField] private float rangeX = 1f;
+    [SerializeField] private float rangeY = 4f;
+
+    private Transform player;
+    private bool isAggressive = false;
+    private Vector3 targetPos;
 
     public float Speed {
-        get { return speed; }
-        set { speed = value; }
+        get { return standardSpeed; }
+        set { standardSpeed = value; }
+    }
+    public bool IsAggressive {
+        get { return isAggressive; }
+        set { isAggressive = value; }
     }
 
     private float topOfLevel = 9;
@@ -21,11 +32,50 @@ public class EnemyController2D : MonoBehaviour {
         bottomOfLevel = Mathf.Min(0, bottomOfLevel);
     }
 
-    protected virtual void CalculateMovement() {
-        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
-        if (transform.position.y <= BottomOfLevel) {
-            float randX = Random.Range((float) -10, 10);
-            transform.position = new Vector3(randX, 9, 0);
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //continuously check the distance to the player
+        StartCoroutine(CheckDistanceToPlayer());
+    }
+
+    private void Update() {
+        //If isAggressive is true, then currentSpeed will be set to standardSpeed *
+        //aggressionSpeedMultiplier. Otherwise, if isAggressive is false, currentSpeed will
+        //be set to standardSpeed.
+        currentSpeed = IsAggressive ? standardSpeed * aggressionSpeedMultiplier : standardSpeed;
+        transform.Translate(Vector3.down * currentSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator CheckDistanceToPlayer() {
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        while (true) {
+            //calculate the distance to the player
+            float distanceX = Mathf.Abs(player.position.x - transform.position.x);
+            float distanceY = Mathf.Abs(player.position.y - transform.position.y);
+
+            //check if the player is within the aggression range
+            if (distanceX <= rangeX && distanceY <= rangeY) {
+                if (!IsAggressive) {
+                    //enemy enters the range and becomes aggressive
+                    IsAggressive = true;
+                    Debug.Log("Enemy is aggressive!");
+                }
+            } else {
+                if (IsAggressive) {
+                    //enemy is out of range, return to standard speed
+                    IsAggressive = false;
+                    Debug.Log("Enemy is no longer aggressive.");
+                }
+            }
+            yield return wait;
         }
     }
+
+    //protected virtual void CalculateMovement() {
+    //    transform.Translate(Vector3.down * standardSpeed * Time.deltaTime, Space.World);
+    //    if (transform.position.y <= BottomOfLevel) {
+    //        float randX = Random.Range((float) -10, 10);
+    //        transform.position = new Vector3(randX, 9, 0);
+    //    }
+    //}
 }
--- Assets/Scripts/Controllers/Enemies/SpinnerController2D.cs
+++ Assets/Scripts/Controllers/Enemies/SpinnerController2D.cs
@@ -3,12 +3,12 @@
 public class SpinnerController2D : EnemyController2D {
     [SerializeField] private float rotateSpeed;
     
-    void Update() {
-        CalculateMovement();
-    }
+    //void Update() {
+    //    CalculateMovement();
+    //}
 
-    protected override void CalculateMovement() {
-        base.CalculateMovement();
-        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
-    }
+    //protected override void CalculateMovement() {
+    //    base.CalculateMovement();
+    //    transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    //}
 }
