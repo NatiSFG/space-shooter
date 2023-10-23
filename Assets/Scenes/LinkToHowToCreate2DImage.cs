1474e5d12d46a8d4385ff9961a31acfa216c6213
Natalia DaLomba <natalia@starforce.games>
2023-10-23 16:32:41 -0400

    Charger enemy prefab set up and sspawning correct amount each wave. Damages player and player can kill enemy. The Charger enemy speeds up when close to player and slows down when away. Need to have enemy loop once hitting the bottom of the screen

---
+++ Assets/Scenes/LinkToHowToCreate2DImage.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class LinkToHowToCreate2DImage : MonoBehaviour {
+    //https://pudp.gamedevhq.com/2d-games-development-production-complete/creating-custom-2d-assets-using-filebase-c/
+}
--- Assets/Scripts/Controllers/Enemies/EnemyController2D.cs
+++ Assets/Scripts/Controllers/Enemies/EnemyController2D.cs
@@ -2,22 +2,12 @@
 using UnityEngine;
 
 public class EnemyController2D : MonoBehaviour {
-    [SerializeField] private float currentSpeed;
+    
     [SerializeField] protected float standardSpeed = 3f;
-    [SerializeField] private float aggressionSpeedMultiplier = 1.25f;
-    [SerializeField] private float rangeX = 1f;
-    [SerializeField] private float rangeY = 4f;
-
-    private Transform player;
-    private bool isAggressive = false;
 
     public float Speed {
         get { return standardSpeed; }
         set { standardSpeed = value; }
-    }
-    public bool IsAggressive {
-        get { return isAggressive; }
-        set { isAggressive = value; }
     }
 
     private float topOfLevel = 9;
@@ -29,45 +19,6 @@ public class EnemyController2D : MonoBehaviour {
         topOfLevel = Mathf.Max(0, topOfLevel);
         //bottomOfLevel will always be 0 or negative, the minimum between itself and 0.
         bottomOfLevel = Mathf.Min(0, bottomOfLevel);
-    }
-
-    private void Start() {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        //continuously check the distance to the player
-        StartCoroutine(CheckDistanceToPlayer());
-    }
-
-    private void Update() {
-        //If isAggressive is true, then currentSpeed will be set to standardSpeed *
-        //aggressionSpeedMultiplier. Otherwise, if isAggressive is false, currentSpeed will
-        //be set to standardSpeed.
-        currentSpeed = IsAggressive ? standardSpeed * aggressionSpeedMultiplier : standardSpeed;
-        transform.Translate(Vector3.down * currentSpeed * Time.deltaTime);
-    }
-
-    private IEnumerator CheckDistanceToPlayer() {
-        WaitForSeconds wait = new WaitForSeconds(0.5f);
-        while (true) {
-            //calculate the distance to the player
-            float distanceX = Mathf.Abs(player.position.x - transform.position.x);
-            float distanceY = Mathf.Abs(player.position.y - transform.position.y);
-
-            //check if the player is within the aggression range
-            if (distanceX <= rangeX && distanceY <= rangeY) {
-                if (!IsAggressive) {
-                    //enemy enters the range and becomes aggressive
-                    IsAggressive = true;
-                    Debug.Log("Enemy is aggressive!");
-                }
-            } else {
-                if (IsAggressive) {
-                    //enemy is out of range, return to standard speed
-                    IsAggressive = false;
-                    Debug.Log("Enemy is no longer aggressive.");
-                }
-            }
-            yield return wait;
-        }
     }
 
     protected virtual void CalculateMovement() {
