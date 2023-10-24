2491d77e6106ffab7d2e7ce4e928a1fb39e27a34
Natalia DaLomba <natalia@starforce.games>
2023-10-23 22:50:15 -0400

    Back Shooter now shoots only when player is above and in line. All that's left is to integrate the enemy into the wave system

--- Assets/Scripts/Enemies/BackShooterEnemy.cs
+++ Assets/Scripts/Enemies/BackShooterEnemy.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class BackShooterEnemy : Enemy {
     [SerializeField] GameObject laserPrefab;
+    [SerializeField] private float rangeX = 1f;
     
     private BackShooterController2D controller;
     private WaveSystem waveSystem;
     private EnemyInfo enemyInfo;
 
+    private Transform player;
+    private bool isPlayerAboveAndInLine = false;
 
+    public bool IsPlayerAboveAndInLine {
+        get { return isPlayerAboveAndInLine; }
+        set { isPlayerAboveAndInLine = value; }
+    }
 
     private new void Start() {
         base.Start();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         enemyInfo = new EnemyInfo(laserPrefab, controller, Random.Range(2, 5), -1);
         controller = GetComponent<BackShooterController2D>();
         waveSystem = FindObjectOfType<WaveSystem>();
-    }
 
-    private void Update() {
-        FireLaser();
+        //continuously check if the player is within range
+        StartCoroutine(CheckIfPlayerInRange());
     }
 
     protected override void FireLaser() {
-        if (Time.time > enemyInfo.canFire && isAlive && !waveSystem.IsPlayerDefeated) {
+        if (Time.time > enemyInfo.canFire && isAlive && !waveSystem.IsPlayerDefeated && IsPlayerAboveAndInLine) {
             enemyInfo.canFire = Time.time + enemyInfo.fireRate;
             GameObject enemyLaser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
             Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
             for (int i = 0; i < lasers.Length; i++)
                 lasers[i].AssignBackShooterLaser();
+        }
+    }
+
+    private IEnumerator CheckIfPlayerInRange() {
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        while (true) {
+            //calculate the distance to the player
+            float distanceX = Mathf.Abs(player.position.x - transform.position.x);
+
+            //check if the player is within line of shooting
+            if (distanceX <= rangeX && transform.position.y < player.position.y) {
+                if (!IsPlayerAboveAndInLine) {
+                    IsPlayerAboveAndInLine = true;
+                    FireLaser();
+                }
+            } else {
+                if (IsPlayerAboveAndInLine)
+                    IsPlayerAboveAndInLine = false;
+            } yield return wait;
         }
     }
 }
