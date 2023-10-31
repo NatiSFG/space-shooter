acf71be24945aa7e75e90bfa5b53a7f1a42b1bbb
Natalia DaLomba <natalia@starforce.games>
2023-10-31 09:35:44 -0400

    Dodger enemy can dodge to the left and right now

--- Assets/Scripts/Enemies/Shared Behaviours/DodgeLaser.cs
+++ Assets/Scripts/Enemies/Shared Behaviours/DodgeLaser.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class DodgeLaser : MonoBehaviour {
     [SerializeField] private float dodgeDistance = 2f;
     [SerializeField] private float speed = 5f;
-    [SerializeField] private Transform movementTarget;
+    [SerializeField] private Transform dodgerEnemy;
 
     private EnemyWaveSpawner enemyWaveSpawner;
     private Transform lastNoticedLaser;
@@ -17,9 +17,9 @@ public class DodgeLaser : MonoBehaviour {
 
     private void Update() {
         if (lastNoticedLaser != null) {
-            Vector3 currentPos = movementTarget.position;
+            Vector3 currentPos = dodgerEnemy.position;
             currentPos.x = Mathf.Lerp(currentPos.x, targetPos.x, speed * Time.deltaTime);
-            movementTarget.position = currentPos;
+            dodgerEnemy.position = currentPos;
             if (Mathf.Abs(currentPos.x - targetPos.x) < 0.001f) {
                 lastNoticedLaser = null;
             }
@@ -29,9 +29,12 @@ public class DodgeLaser : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player Laser") {
             lastNoticedLaser = other.transform;
-            lastDodgeStartPos = movementTarget.position;
+            lastDodgeStartPos = dodgerEnemy.position;
             targetPos = lastDodgeStartPos;
-            targetPos.x -= dodgeDistance; //move to the left. add right logic later
+            int direction = Random.Range(0, 2);
+            if (direction == 0)
+                targetPos.x -= dodgeDistance; //move to the left
+            else targetPos.x += dodgeDistance; //move to the right
         }
     }
 }
