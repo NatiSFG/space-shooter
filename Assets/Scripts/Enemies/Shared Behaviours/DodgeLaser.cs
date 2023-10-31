1081c11a9b4ab1af4992c37216ea057d0cb32937
Natalia DaLomba <natalia@starforce.games>
2023-10-31 09:57:32 -0400

    Enemy limited to the level bounds

--- Assets/Scripts/Enemies/Shared Behaviours/DodgeLaser.cs
+++ Assets/Scripts/Enemies/Shared Behaviours/DodgeLaser.cs
@@ -4,37 +4,39 @@ using UnityEngine;
 public class DodgeLaser : MonoBehaviour {
     [SerializeField] private float dodgeDistance = 2f;
     [SerializeField] private float speed = 5f;
-    [SerializeField] private Transform dodgerEnemy;
+    [SerializeField] private Transform dodger;
 
-    private EnemyWaveSpawner enemyWaveSpawner;
+    private EnemyWaveSpawner spawner;
     private Transform lastNoticedLaser;
     private Vector3 lastDodgeStartPos;
     private Vector3 targetPos;
 
     private void Start() {
-        enemyWaveSpawner = Object.FindObjectOfType<EnemyWaveSpawner>();
+        spawner = Object.FindObjectOfType<EnemyWaveSpawner>();
     }
 
     private void Update() {
         if (lastNoticedLaser != null) {
-            Vector3 currentPos = dodgerEnemy.position;
+            Vector3 currentPos = dodger.position;
             currentPos.x = Mathf.Lerp(currentPos.x, targetPos.x, speed * Time.deltaTime);
-            dodgerEnemy.position = currentPos;
-            if (Mathf.Abs(currentPos.x - targetPos.x) < 0.001f) {
+            dodger.position = currentPos;
+            if (Mathf.Abs(currentPos.x - targetPos.x) < 0.001f)
                 lastNoticedLaser = null;
-            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player Laser") {
             lastNoticedLaser = other.transform;
-            lastDodgeStartPos = dodgerEnemy.position;
+            lastDodgeStartPos = dodger.position;
             targetPos = lastDodgeStartPos;
             int direction = Random.Range(0, 2);
-            if (direction == 0)
-                targetPos.x -= dodgeDistance; //move to the left
-            else targetPos.x += dodgeDistance; //move to the right
+            float leftSpaceAvailable = Mathf.Abs(spawner.MinXSpawnPoint - lastDodgeStartPos.x);
+            float rightSpaceAvailable = Mathf.Abs(spawner.MaxXSpawnPoint - lastDodgeStartPos.x);
+            if (direction == 0 && leftSpaceAvailable >= dodgeDistance)
+                targetPos.x -= dodgeDistance; //new x value to move to the left
+            else if (direction == 1 && rightSpaceAvailable >= dodgeDistance)
+                targetPos.x += dodgeDistance; //new x value to move to the right
         }
     }
 }
